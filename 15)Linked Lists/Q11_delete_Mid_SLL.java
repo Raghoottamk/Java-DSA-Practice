b7347e5c7e9151f4f8363256ca8c747ab382cb45
Raghoottamk <122477613+Raghoottamk@users.noreply.github.com>
2023-12-17 22:33:57 +0530

    delete mid of SLL

---
+++ 15)Linked Lists/Q11_delete_Mid_SLL.java
@@ -0,0 +1,22 @@
+
+public class Q11_delete_Mid_SLL {
+    //Definition for singly-linked list.
+    public class ListNode {
+        int val;
+        ListNode next;
+        ListNode() {}
+        ListNode(int val) { this.val = val; }
+        ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+    }
+    public ListNode deleteMiddle(ListNode head) {
+        if(head == null || head.next == null) return null;
+        ListNode fast = head.next.next;
+        ListNode slow = head;
+        while(fast != null && fast.next != null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        slow.next = slow.next.next;
+        return head;
+    }
+}
