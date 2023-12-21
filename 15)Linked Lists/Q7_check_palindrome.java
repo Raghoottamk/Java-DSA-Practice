3d381c47c334d2d8ad360bcd1a92dd0a6a7beb8f
Raghoottamk <122477613+Raghoottamk@users.noreply.github.com>
2023-12-21 22:45:23 +0530

    delete Nodes if next greater exists iterative O(n^2),using stack,recursive

--- 15)Linked Lists/Q7_check_palindrome.java
+++ 15)Linked Lists/Q7_check_palindrome.java
@@ -11,6 +11,8 @@ public class Q7_check_palindrome {
         }
     }
     //brute force
+    //TC = O(3/2 n) 
+    //SC = O(n)
     public boolean isPalindrome(ListNode head) {
         ListNode temp = head;
         Stack<Integer> stack = new Stack<>();
@@ -26,6 +28,30 @@ public class Q7_check_palindrome {
         }
         return true;
     }
+    //TC = O(n) 
+    //SC = O(n/2)
+    public boolean isPalindrome3(ListNode head) {
+        ListNode fast = head;
+        ListNode slow = head;
+        Stack<Integer> s = new Stack<>();
+        while(fast != null && fast.next != null){
+            s.push(slow.val);
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+        if(fast!=null && fast.next == null){
+            //odd case
+            slow = slow.next;
+        }
+        while(!s.isEmpty()){
+            if(s.pop() != slow.val){
+                return false;
+            }
+            slow = slow.next;
+        }
+        return true;
+    }
+
     //optimal
     public boolean isPalindrome2(ListNode head) {
         if(head == null || head.next == null) return true;
