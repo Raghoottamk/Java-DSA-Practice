510c05f9f2a376bf5e499c60d814148388aa53b9
Raghoottamk <122477613+Raghoottamk@users.noreply.github.com>
2023-12-24 22:05:26 +0530

    postorder iterative traversal

--- 18)BinaryTree/Q3_iterative_postorder.java
+++ 18)BinaryTree/Q3_iterative_postorder.java
@@ -13,58 +13,30 @@ public class Q3_iterative_postorder {
     }
     public static class BinaryTree{
         Node root;
-        ArrayList<Integer> list = new ArrayList<Integer>();
-    
         // An iterative function to do postorder traversal
         // of a given binary tree
-        ArrayList<Integer> postOrderIterative(Node node){
-            Stack<Node> S = new Stack<Node>();
-    
-            // Check for empty tree
-            if (node == null)
-                return list;
-            S.push(node);
-            Node prev = null;
-            while (!S.isEmpty()) {
-                Node current = S.peek();
-    
-                /* go down the tree in search of a leaf an if so
-                process it and pop stack otherwise move down */
-                if (prev == null || prev.left == current || prev.right == current) {
-                    if (current.left != null)
-                        S.push(current.left);
-                    else if (current.right != null)
-                        S.push(current.right);
-                    else {
-                        S.pop();
-                        list.add(current.data);
-                    }
-    
-                    /* go up the tree from left node, if the
-                    child is right push it onto stack otherwise
-                    process parent and pop stack */
+        public static List<Integer> postorder(Node root){
+            List<Integer> list = new ArrayList<>();
+            Stack<Node> stack = new Stack<>();
+            while(true) {
+                while(root != null) {
+                    stack.push(root);
+                    stack.push(root);
+                    root = root.left;
                 }
-                else if (current.left == prev) {
-                    if (current.right != null)
-                        S.push(current.right);
-                    else {
-                        S.pop();
-                        list.add(current.data);
-                    }
-    
-                    /* go up the tree from right node and after
-                    coming back from right node process parent
-                    and pop stack */
+                
+                // Check for empty stack
+                if(stack.empty()) return list;
+                root = stack.pop();
+                
+                if(!stack.empty() && stack.peek() == root) root = root.right;
+                
+                else {
+                    list.add(root.data);
+                    root = null;
                 }
-                else if (current.right == prev) {
-                    S.pop();
-                    list.add(current.data);
-                }
-    
-                prev = current;
             }
-    
-            return list;
+
         }
     }
     public static void main(String args[]){
@@ -76,10 +48,7 @@ public class Q3_iterative_postorder {
         tree.root.left.right = new Node(5);
         tree.root.right.left = new Node(2);
  
-        ArrayList<Integer> mylist = tree.postOrderIterative(tree.root);
- 
-        System.out.println(
-            "Post order traversal of binary tree is :");
-        System.out.println(mylist);
+        System.out.println("Post order traversal of binary tree is :");
+        System.out.println(tree.postorder(tree.root));//[3, 5, 8, 2, 17, 10]
     }
 }
