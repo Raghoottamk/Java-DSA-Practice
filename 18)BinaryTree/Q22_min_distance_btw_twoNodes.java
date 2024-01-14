3b15fd1c99055c34ba6efde9a113034295717ea6
Raghoottamk <122477613+Raghoottamk@users.noreply.github.com>
2024-01-14 18:39:13 +0530

    minimum distance between two nodes using LCA approach

--- 18)BinaryTree/Q22_min_distance_btw_twoNodes.java
+++ 18)BinaryTree/Q22_min_distance_btw_twoNodes.java
@@ -1,4 +1,29 @@
 public class Q22_min_distance_btw_twoNodes {
+/*
+--Question--
+Given a binary tree and two node values, a and b, your task is to find the minimum distance between them. The given two nodes are guaranteed to be in the binary tree and all node values are unique.
+Example 1:
+Input:
+        1
+      /  \
+     2    3
+a = 2, b = 3
+Output: 2
+Explanation: 
+We need the distance between 2 and 3. Being at node 2, we need to take two steps ahead in order to reach node 3. The path followed will be: 2 -> 1 -> 3. Hence, the result is 2. 
+
+Example 2:
+Input:
+        11
+      /   \
+     22  33
+    /  \  /  \
+  44 55 66 77
+a = 77, b = 22
+Output: 3
+Explanation: 
+We need the distance between 77 and 22. Being at node 77, we need to take three steps ahead in order to reach node 22. The path followed will be: 77 -> 33 -> 11 -> 22. Hence, the result is 3.
+ */
 
     class Node{
         int data;
