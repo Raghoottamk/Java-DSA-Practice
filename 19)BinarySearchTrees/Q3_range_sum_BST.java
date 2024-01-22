619dc5a19ea4e926fa5cd89e594a0903192c31d0
Raghoottamk <122477613+Raghoottamk@users.noreply.github.com>
2024-01-22 12:20:05 +0530

    range of BST provided two numbers high and low

--- 19)BinarySearchTrees/Q3_range_sum_BST.java
+++ 19)BinarySearchTrees/Q3_range_sum_BST.java
@@ -34,4 +34,22 @@ public class Q3_range_sum_BST {
             }
         }
     }
+    //without using helper func
+    class Solution2 {
+        public int rangeSumBST(TreeNode root, int low, int high) {
+            if (root == null) {
+                return 0;
+            }
+          
+            if (low <= root.val && root.val <= high) {
+                return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+            } 
+            else if (root.val < low) {
+                return rangeSumBST(root.right, low, high);
+            } 
+            else {
+                return rangeSumBST(root.left, low, high);
+            }
+        }
+    }
 }
