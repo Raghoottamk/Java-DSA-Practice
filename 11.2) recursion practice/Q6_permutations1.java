5deb0ac0a3d7440c0240c1dc2a5510d1d0e1ff7c
Raghoottamk <122477613+Raghoottamk@users.noreply.github.com>
2024-01-23 23:56:45 +0530

    permutation

--- 11.2) recursion practice/Q6_permutations1.java
+++ 11.2) recursion practice/Q6_permutations1.java
@@ -1,6 +1,20 @@
 import java.util.ArrayList;
 import java.util.List;
+/*
+Given an array nums of distinct integers, return all the possible permutations. You can return the answer in any order.
 
+Example 1:
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+
+Example 2:
+Input: nums = [0,1]
+Output: [[0,1],[1,0]]
+
+Example 3:
+Input: nums = [1]
+Output: [[1]]
+ */
 public class Q6_permutations1 {
     class Solution {
         public List<List<Integer>> permute(int[] nums) {
--- 11.2) recursion practice/Q7_permutation2.java
+++ 11.2) recursion practice/Q7_permutation2.java
@@ -1,8 +1,29 @@
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
+/*
+Question:
+Given a collection of numbers, nums, that might contain duplicates, return all possible unique permutations in any order.
 
+Example 1:
+Input: nums = [1,1,2]
+Output:
+[[1,1,2],
+ [1,2,1],
+ [2,1,1]]
+
+Example 2:
+Input: nums = [1,2,3]
+Output: [[1,2,3],[1,3,2],[2,1,3],[2,3,1],[3,1,2],[3,2,1]]
+ 
+Constraints:
+1 <= nums.length <= 8
+-10 <= nums[i] <= 10
+
+ */
 public class Q7_permutation2 {
+    //brute force
     class Solution {
         public List<List<Integer>> permuteUnique(int[] nums) {
             List<List<Integer>> list=new ArrayList<>(); 
@@ -26,6 +47,30 @@ public class Q7_permutation2 {
                     helper(nums,list,currlist,map);
                     currlist.remove(currlist.size()-1);
                     map.remove(j);
+                }
+            }
+        }
+    }
+    //optimized
+    class Solution2 {
+        public List<List<Integer>> permuteUnique(int[] nums) {
+            List<List<Integer>> list = new ArrayList<>();
+            Arrays.sort(nums);
+            backtrack4(list, new ArrayList<>(), nums, new boolean[nums.length]);
+            return list;
+        }
+
+        private void backtrack4(List<List<Integer>> list, List<Integer> tempList, int [] nums, boolean []bool){
+            if(tempList.size() == nums.length){
+                list.add(new ArrayList<>(tempList));
+            } else{
+                for(int i = 0; i < nums.length; i++){
+                    if(bool[i] || i > 0 && nums[i] == nums[i-1] && !bool[i - 1]) continue;
+                    bool[i] = true; 
+                    tempList.add(nums[i]);
+                    backtrack4(list, tempList, nums, bool);
+                    bool[i] = false; 
+                    tempList.remove(tempList.size() - 1);
                 }
             }
         }
