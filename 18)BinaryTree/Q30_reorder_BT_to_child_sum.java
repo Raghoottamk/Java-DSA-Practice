ec8e043f7a40608435d463f7c30731ae001fce39
Raghoottamk <122477613+Raghoottamk@users.noreply.github.com>
2024-01-28 17:57:53 +0530

    reorder given BT to child sum property

---
+++ 18)BinaryTree/Q30_reorder_BT_to_child_sum.java
@@ -0,0 +1,42 @@
+public class Q30_reorder_BT_to_child_sum {
+    //striver sheet
+    
+    class Node {
+        int data;
+        Node  left,  right;
+        Node(int data)
+        {
+            this.data=data;
+            left=null;
+            right=null;
+        }
+    }
+    class TUF{
+        static void reorder(Node  root) {
+        if (root == null) return;
+        int child = 0;
+        if (root . left!=null) {
+            child += root . left . data;
+        }
+        if (root . right!=null) {
+            child += root . right . data;
+        }
+
+        if (child < root . data) {
+            if (root . left!=null) root . left . data = root . data;
+            else if (root . right!=null) root . right . data = root . data;
+        }
+
+        reorder(root . left);
+        reorder(root . right);
+
+        int tot = 0;
+        if (root . left!=null) tot += root . left . data;
+        if (root . right!=null) tot += root . right . data;
+        if (root . left!=null || root . right!=null) root . data = tot;
+        }
+        static void changeTree(Node  root) {
+        reorder(root);
+        }
+    }
+}
