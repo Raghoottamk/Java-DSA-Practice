eaf574c22f8277f8cd6be1d0503afb545a4dfe13
Raghoottamk <122477613+Raghoottamk@users.noreply.github.com>
2024-02-08 06:28:53 +0530

    frog jumps with k jumps

---
+++ 23)Dynamic Programming/Leetcode_extra_quest/Q5_frog_jumps_with_k_jumps.java
@@ -0,0 +1,38 @@
+package Leetcode_extra_quest;
+
+import java.util.Arrays;
+
+public class Q5_frog_jumps_with_k_jumps {
+    public class Solution {
+    public static int minimizeCost(int n, int k, int []height){
+        // Write your code here.
+        int dp[] = new int[n];
+        Arrays.fill(dp,-1);
+        return solveUtil(n - 1, height, dp, k);
+    }
+    static int solveUtil(int ind, int[] height, int[] dp, int k) {
+        // Base case: If we are at the beginning (index 0), no cost is needed.
+        if (ind == 0)
+            return 0;
+
+        // If the result for this index has been previously calculated, return it.
+        if (dp[ind] != -1)
+            return dp[ind];
+
+        int mmSteps = Integer.MAX_VALUE;
+
+        // Loop to try all possible jumps from '1' to 'k'
+        for (int j = 1; j <= k; j++) {
+            // Ensure that we do not jump beyond the beginning of the array
+            if (ind - j >= 0) {
+                // Calculate the cost for this jump and update mmSteps with the minimum cost
+                int jump = solveUtil(ind - j, height, dp, k) + Math.abs(height[ind] - height[ind - j]);
+                mmSteps = Math.min(jump, mmSteps);
+            }
+        }
+
+        // Store the minimum cost for this index in the dp array and return it.
+        return dp[ind] = mmSteps;
+    }
+}
+}
