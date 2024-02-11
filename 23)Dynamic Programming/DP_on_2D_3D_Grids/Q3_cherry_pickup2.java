461a04f6bcf997bc3859f834d7ccedc31856fbe8
Raghoottamk <122477613+Raghoottamk@users.noreply.github.com>
2024-02-11 19:05:54 +0530

    cherry pickup II LC hard question 1463 memoization,tabulation

--- 23)Dynamic Programming/DP_on_2D_3D_Grids/Q3_cherry_pickup2.java
+++ 23)Dynamic Programming/DP_on_2D_3D_Grids/Q3_cherry_pickup2.java
@@ -1,9 +1,16 @@
 import java.util.Arrays;
 //https://leetcode.com/problems/cherry-pickup-ii/?envType=daily-question&envId=2024-02-11
 public class Q3_cherry_pickup2 {
-    //recursion
+/*recursion 
+    TC=O(9)^(rows*cols*cols)
+    sc = O(rows)
+*/
 
-class Solution2 {
+/*memoization
+    TC = 9*(rows*cols*cols) 
+    SC = O(rows**cols*cols)dp array + O(rows)stack space
+*/
+class Solution1 {
     public int cherryPickup(int[][] grid) {
         int row = grid.length;
         int col = grid[0].length;
@@ -45,6 +52,49 @@ class Solution2 {
         ans += max;
         
         return dp[currRow][Acol][Bcol] = ans;
+    }
+}
+
+/*Tabulation
+    TC = 9(rows*cols*cols)
+    base =
+ */
+class Solution2 {
+    public int cherryPickup(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+
+        int[][][] dp = new int[m][n][n];
+        int ans = 0;
+
+        for(int i=0;i<m;i++)
+            for(int j=0;j<n;j++)
+                Arrays.fill(dp[i][j], -1);
+
+        dp[0][0][n-1] = grid[0][0] + grid[0][n-1];
+        //other indices for dp[0][0][x] not required coz in first row we can pick only the first 0th and n-1 col cherries
+
+        for(int i=1;i<m;i++) {
+            for(int j=0;j<n;j++) { // robotA
+                for(int k=j+1;k<n;k++) { // robotB
+                    int max = -1;
+                    for(int x=-1;x<=1;x++) { // we will have total 9 combinations
+                        for(int y=-1;y<=1;y++) {
+                            if(j+x >= 0 && j+x < n && k+y >= 0 && k+y < n){
+                                if(j+x < k+x){//avoid cross over as it does n't make any sense
+                                    max = Math.max(max,dp[i-1][j+x][k+y]);
+                                }
+                            }
+                        }
+                    }
+                    if(max != -1)
+                        dp[i][j][k] = max + grid[i][j] + grid[i][k];
+                    if(ans < dp[i][j][k]) ans = dp[i][j][k];
+                }
+            }
+        }
+
+        return ans;
     }
 }
 }
