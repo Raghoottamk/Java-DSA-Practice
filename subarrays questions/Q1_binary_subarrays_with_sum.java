e83e3cda23519565d0caf5eb95241026f0b21e98
Raghoottamk <122477613+Raghoottamk@users.noreply.github.com>
2024-03-18 15:46:05 +0530

    binary subarray question

---
+++ subarrays questions/Q1_binary_subarrays_with_sum.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Q1_binary_subarrays_with_sum {
+//https://leetcode.com/problems/binary-subarrays-with-sum/description/
+    class Solution {
+        public int numSubarraysWithSum(int[] nums, int goal) {
+            int sum = 0;
+            int cnt = 0;
+            HashMap<Integer, Integer> map = new HashMap<>();
+            map.put(0,1);
+            for(int i = 0 ; i < nums.length ; i++){
+                sum += nums[i];
+                int rem = sum - goal;
+                if(map.containsKey(rem)){
+                    cnt += map.get(rem);
+                }
+                map.put(sum,map.getOrDefault(sum,0)+1);
+            }
+            return cnt;
+        }
+    }
+}
